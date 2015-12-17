0d745b9adbc6cf0d656743f3daa45498a137bdd5
ben willkommen <benwillkommen@gmail.com>
2015-12-17 14:23:53 -0600

    fixed bug with jokes cache

--- bill-bot.ps1
+++ bill-bot.ps1
@@ -13,11 +13,11 @@ $requestCount = 0 + (Get-Content $requestCountFileName)
 
 if(-not (Test-Path $jokesCacheFileName))
 {
-    $defaultJokes = "['A man received a phone call one day, and the caller asked if he had lost a parrot. He said that he had indeed lost the bird, but wanted to know how the caller located him.The called said that the bird had landed on his balcony and kept repeating, \'Hi, you have reached 555-1234. I can`t come to the phone right now, please leave a message at the tone.\'', 'When Chuck Norris was denied a Bacon McMuffin at McDonalds because it was 10:35, he roundhouse kicked the store so hard it became a KFC.']"
+    $defaultJokes = "['A man received a phone call one day, and the caller asked if he had lost a parrot. He said that he had indeed lost the bird, but wanted to know how the caller located him.The called said that the bird had landed on his balcony and kept repeating, \'Hi, you have reached 555-1234. I can\'t come to the phone right now, please leave a message at the tone.\'']"
     $defaultJokes | Out-File -FilePath $jokesCacheFileName
 }
 
-$cachedJokes = Get-Content $jokesCacheFileName | ConvertFrom-Json
+$cachedJokes = Get-Content -Path $jokesCacheFileName | ConvertFrom-Json
 
 if($requestCount -ge 8)
 {
@@ -33,7 +33,7 @@ else{
     $requestCount | out-file -FilePath $requestCountFileName
 
     $joke = ($response | Convertfrom-Json).joke
-    $cachedJokes + $joke | ConvertTo-Json | Out-File -FilePath $jokesCacheFileName
+    $cachedJokes + $joke | ConvertTo-Json -Compress | Out-File -FilePath $jokesCacheFileName
 
     Write-Host $joke 
     $speak.Speak($joke)
